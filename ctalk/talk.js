c4e4bed8f73c78b798473573b17dc7956a3c063a
kraggio <119532707+kraggio@users.noreply.github.com>
2023-01-17 12:00:49 -0500

    Add files via upload

---
+++ ctalk/talk.js
@@ -0,0 +1,10 @@
+function calculateCost() {
+    let name = 'rupert'
+    document.getElementById("name").value;
+    let characterPrice = (name.length)
+    document.getElementById("character-price").value;
+    let cost = name.length * characterPrice;
+    alert(`The cost to print "${name}" on a billboard is ${cost} dollars.`);
+}
+
+console.log(characterPrice * 25)
